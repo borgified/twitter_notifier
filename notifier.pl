cd8efebf99b47f3d2be837ab2431bf6fd5f5fa9d
Fwiffo <borgified@gmail.com>
2013-09-19 16:15:14 -0700

    initial commit

---
+++ notifier.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use Net::Twitter;
+use Scalar::Util 'blessed';
+
+my %config = do '/secret/twitter.config';
+
+my $consumer_key		= $config{consumer_key};
+my $consumer_secret		= $config{consumer_secret};
+my $token			= $config{token};
+my $token_secret		= $config{token_secret};
+
+my $nt = Net::Twitter->new(
+	traits   => [qw/OAuth API::REST/],
+	consumer_key        => $consumer_key,
+	consumer_secret     => $consumer_secret,
+	access_token        => $token,
+	access_token_secret => $token_secret,
+);
+
+my %gistconfig = do '/tmp/gists';
+
+my($build,$worker)=@_;
+$nt->update("build #$build has finished on $worker. check it out https://gist.github.com/borgified/$gistconfig{$worker}");
+
+if ( my $err = $@ ) {
+	die $@ unless blessed $err && $err->isa('Net::Twitter::Error');
+
+	warn "HTTP Response Code: ", $err->code, "\n",
+	"HTTP Message......: ", $err->message, "\n",
+	"Twitter error.....: ", $err->error, "\n";
+}
+
